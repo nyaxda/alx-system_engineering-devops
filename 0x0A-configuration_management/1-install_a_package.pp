7215ad309a5ae87f1ff28eae9705e66c13a65ae6
Daniel Nyakundi <nyaxda@gmail.com>
2024-04-19 15:25:46 -0700

    Errorfix

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -3,8 +3,9 @@ package { 'python3-pip':
   ensure => installed,
 }
 
-package { 'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3',
-  require  => Package['python3-pip'],
+# Using Puppet, install flask globally from pip3
+exec { 'install-flask':
+  command => '/usr/bin/pip3 install flask==2.1.0',
+  require => Package['python3-pip'],
+  path    => ['/bin', '/usr/bin'],
 }
