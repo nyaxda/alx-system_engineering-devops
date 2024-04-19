5cd41cd5811d1802db95d399ab79c88f53ed9be6
Daniel Nyakundi <nyaxda@gmail.com>
2024-04-19 15:17:17 -0700

    Trial

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,9 +1,16 @@
-# Using Puppet, install flask from pip3
+# installing a package
+
+exec { 'apt-get update':
+ command => '/usr/bin/apt-get update'
+ path    => ['/usr/bin'],
+ before  => Package['python3-pip'],
+}
+
 package { 'python3-pip':
   ensure => installed,
 }
 
-package { 'flask':
+package { 'Flask':
   ensure   => '2.1.0',
   provider => 'pip3',
   require  => Package['python3-pip'],
