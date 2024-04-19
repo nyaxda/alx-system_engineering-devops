e39bcf1a81432fae10a7468fda157da4ecfcd26d
Daniel Nyakundi <nyaxda@gmail.com>
2024-04-19 14:51:31 -0700

    Changes

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,5 @@
-#!/usr/bin/pup
-# installing flask
-
+# Using Puppet, install flask from pip3
 package { 'flask':
-    ensure      => '2.1.0',
-    provider    => 'pip3',    
+  ensure   => '2.1.0',
+  provider => 'pip3',
 }
