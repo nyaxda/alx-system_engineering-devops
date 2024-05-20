abe1f171ef1eba96b2faad1e10230253254625b7
Daniel Nyakundi <nyaxda@gmail.com>
2024-05-20 16:46:30 -0700

    chore: Refactor API data retrieval and display code

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -2,7 +2,7 @@
 """Returns to-do list information for a given employee ID."""
 import json
 import sys
-import urllib.request
+import requests
 
 
 if __name__ == "__main__":
@@ -12,12 +12,12 @@ if __name__ == "__main__":
     employee_id = int(sys.argv[1])
     url = 'https://jsonplaceholder.typicode.com'
 
-    response = urllib.request.urlopen("{}/users/{}".format(url, employee_id))
-    employee = json.load(response)
+    response = requests.get("{}/users/{}".format(url, employee_id))
+    employee = response.json()
     employee_name = employee.get('name')
 
-    response2 = urllib.request.urlopen("{}/todos".format(url))
-    todos = json.load(response2)
+    response2 = requests.get("{}/todos".format(url))
+    todos = response2.json()
     filtered_todos = [todo for todo in todos if todo.get(
         "userId") == employee_id]
     total_tasks = len(filtered_todos)
@@ -26,7 +26,7 @@ if __name__ == "__main__":
     done_tasks = len(completed_tasks)
 
     print(
-        "Employee {} is done with tasks ({}/{}):".format(employee_name,
+        "Employee {} is done with tasks({}/{}):".format(employee_name,
                                                          done_tasks,
                                                          total_tasks))
     for i in range(done_tasks):
