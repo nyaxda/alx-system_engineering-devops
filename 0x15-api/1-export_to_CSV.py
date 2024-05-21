68bdb9aa52b3d86615d9ae6f7eca30b72a585d12
NyaxDa <nyaxda@gmail.com>
2024-05-20 18:29:58 -0700

    Tried module documentation

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,15 +1,5 @@
 #!/usr/bin/python3
-"""
-This module exports to-do list information for a given
-employee ID to CSV format.
-"""
-# Using what you did in the task #0,
-# extend your Python script to export data in the  format.
-
-#   Records all tasks that are owned by this employee
-#   Format must be: "USER_ID","USERNAME","TASK_COMPLETED_STATUS","TASK_TITLE"
-#   File name must be: USER_ID.csv
-
+"""Exports File Contents in CSV format"""
 import requests
 import pandas
 import sys
