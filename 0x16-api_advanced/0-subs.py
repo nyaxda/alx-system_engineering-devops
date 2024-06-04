1216648593d28fc9b64c6ac6f04d66cfa84f12c2
Daniel Nyakundi <nyaxda@gmail.com>
2024-06-04 08:01:50 -0700

    Fix subreddit validation in number_of_subscribers function

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -14,6 +14,7 @@ def number_of_subscribers(subreddit):
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {'User-Agent': 'MyApp/1.0'}
     response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 404:
+    try:
+        return response.json().get("data").get("subscribers")
+    except Exception:
         return 0
-    return response.json().get("data").get("subscribers")
