a77bbcebcec37971c3e204b4199d4401932df705
Daniel Nyakundi <nyaxda@gmail.com>
2024-06-04 07:52:19 -0700

    shortened the user agent

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -13,11 +13,7 @@ def number_of_subscribers(subreddit):
     if subreddit is None or not isinstance(subreddit, str):
         return 0
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) '
-        'AppleWebKit/537.36 (KHTML, like Gecko) '
-        'Chrome/124.0.0.0 Safari/537.36'
-        }
+    headers = {'User-Agent': 'MyApp/1.0'}
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 404:
         return 0
