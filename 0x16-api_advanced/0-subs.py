ec2d61f36bdc8d852ec8e6e4ae50fa3ec3e0e5cb
Daniel Nyakundi <nyaxda@gmail.com>
2024-06-04 07:53:46 -0700

    Fix subreddit validation in number_of_subscribers function

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -10,8 +10,6 @@ def number_of_subscribers(subreddit):
     Query the Reddit API and return the number of
     subscribers for a given subreddit
     """
-    if subreddit is None or not isinstance(subreddit, str):
-        return 0
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {'User-Agent': 'MyApp/1.0'}
     response = requests.get(url, headers=headers, allow_redirects=False)
